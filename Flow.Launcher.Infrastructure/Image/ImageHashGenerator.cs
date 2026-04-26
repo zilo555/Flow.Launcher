82e3f742c57e79c511b3b0869ed6f71d863fd19c
benello <behnjaminbonello@fastmail.com>
2026-04-26 17:16:17 +0200

    Make image hashing format agnostic (#4116)

--- Flow.Launcher.Infrastructure/Image/ImageHashGenerator.cs
+++ Flow.Launcher.Infrastructure/Image/ImageHashGenerator.cs
@@ -12,8 +12,6 @@ namespace Flow.Launcher.Infrastructure.Image
 
     public class ImageHashGenerator : IImageHashGenerator
     {
-        private const int HashSize = 64; // Deterministic size for hashing to avoid DPI/stride variability
-
         public string GetHashFromImage(ImageSource imageSource)
         {
             if (imageSource is not BitmapSource { IsFrozen: true } image)
@@ -37,39 +35,15 @@ namespace Flow.Launcher.Infrastructure.Image
                     normalized = converted;
                 }
 
-                // Draw into a fixed-size RenderTarget at 96 DPI to ensure deterministic pixel buffer
-                var rtb = new RenderTargetBitmap(HashSize, HashSize, 96, 96, PixelFormats.Pbgra32);
-                var dv = new DrawingVisual();
-
-                using (var dc = dv.RenderOpen())
-                {
-                    // Uniformly scale to fit into HashSize x HashSize
-                    var scale = Math.Min((double)HashSize / normalized.PixelWidth, (double)HashSize / normalized.PixelHeight);
-                    var drawWidth = normalized.PixelWidth * scale;
-                    var drawHeight = normalized.PixelHeight * scale;
-
-                    var offsetX = (HashSize - drawWidth) / 2;
-                    var offsetY = (HashSize - drawHeight) / 2;
-                    dc.DrawImage(normalized, new System.Windows.Rect(offsetX, offsetY, drawWidth, drawHeight));
-                }
-
-                rtb.Render(dv);
-                rtb.Freeze();
-
-                // Extract raw pixel bytes
-                var bpp = rtb.Format.BitsPerPixel;
+                // Prepare final buffer (should be HashSize x HashSize)
+                var bpp = normalized.Format.BitsPerPixel;
                 if (bpp < 1)
                     return null;
 
                 var bytesPerPixel = (bpp + 7) / 8;
-                var stride = rtb.PixelWidth * bytesPerPixel;
-
-                var bufferSize = stride * rtb.PixelHeight;
-                if (bufferSize < 1)
-                    return null;
-
-                var pixels = new byte[bufferSize];
-                rtb.CopyPixels(pixels, stride, 0);
+                var stride = normalized.PixelWidth * bytesPerPixel;
+                var pixels = new byte[stride * normalized.PixelHeight];
+                normalized.CopyPixels(pixels, stride, 0);
 
                 var hashBytes = SHA1.HashData(pixels);
                 return Convert.ToBase64String(hashBytes);
